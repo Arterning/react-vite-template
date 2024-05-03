1d09c31877e3d76c8f3c04f0d836b2fc7ae08b3e
Arterning <ning.huang.ridson@gmail.com>
2024-05-03 08:16:40 +0800

    login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,27 +1,32 @@
 import { useContext } from "react";
 import AuthContext from "../../contexts/AuthContext";
 
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+
+import "./login.scss";
 
 const Login = () => {
-
   const { login } = useContext(AuthContext);
 
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>Login</h1>
-
-      username: <input type="text" name="username"></input>
-
-      password: <input type="password" name="password"></input>
-      <button onClick={() => { 
-        login()
-        navigate('/')
-      }}>Login</button>
+    <div className="login-page">
+      <form className="form">
+        <h1>Login</h1>
+        <input type="text" name="username"></input>
+        <input type="password" name="password"></input>
+        <button
+          onClick={() => {
+            login();
+            navigate("/");
+          }}
+        >
+          Login
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
